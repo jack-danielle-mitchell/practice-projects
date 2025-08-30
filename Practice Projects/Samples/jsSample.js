abfd9f4e06f7228a7b890d5f93992c154e202bda
Jack Danielle Mitchell <jack.danielle.mitchell@gmail.com>
2025-08-30 16:07:09 -0500

    initial upload & first project: To Do List App
    find To Do List App in firstProject folder

---
+++ Practice Projects/Samples/jsSample.js
@@ -0,0 +1,6 @@
+//to run this file, copy the comment below into your terminal!
+// node jsSample.js
+
+console.log("------------------------------")
+console.log("Hello World! Guess who's back!")
+console.log("------------------------------")
