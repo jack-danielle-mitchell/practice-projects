8b675c6edbb7570b3ab688e770b8313ac0b5ba99
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2025-09-05 10:11:19 -0500

    customized alert for empty input

--- Practice Projects/firstProject/index.js
+++ Practice Projects/firstProject/index.js
@@ -3,7 +3,7 @@ const listContainer = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value===''){
-        alert("You must type something!")
+        document.getElementById("alert").style.display="block";
     } else {
         let li = document.createElement("li");
         li.innerHTML=inputBox.value;
